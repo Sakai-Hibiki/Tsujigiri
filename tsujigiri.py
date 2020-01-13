ce7e4395bdfd3e201bf1161c4b1d71a2171bcca4
Todo-Yurika <59798242+Todo-Yurika@users.noreply.github.com>
2020-01-13 14:29:39 +0900

    Add files via upload

--- tsujigiri.py
+++ tsujigiri.py
@@ -1,16 +1,17 @@
 # coding: utf-8
 
-import sqlite3
 import asyncio
 from datetime import datetime, timedelta
 from contextlib import closing
 import discord
-
+import psycopg2
+import os
 
 DELTA_JST = timedelta(hours=9)
 TOKEN = 'NjY1NTA3ODc2NjIxNTE2ODAw.XhnFhA.Nvbh4jFERg-f1CqD0WGUaNb3BxY'
 client = discord.Client()
 GUILD_ID = 665506952591179781
+dsn = os.environ('DATABASE_URL')
 
 
 # message イベント
@@ -25,16 +26,16 @@ async def on_message(message):
         user_id = message.author.id
         time = utc_to_jst(message.created_at)
         msg_log = (user_id, time)
-        db.write(msg_log)
+        write(msg_log)
 
 # ---commands---
     # activelog を表示する
     if message.content.startswith('.activelog'):
-        await message.channel.send(db.read_all())
+        await message.channel.send(read_all())
     # 本人の前回ログからの経過秒数を表示する
     if message.content.startswith('.term'):
         now = datetime.utcnow() + DELTA_JST
-        for row in db.read_all():
+        for row in read_all():
             term = now - str_to_datetime(row[1])
             await message.channel.send(term.total_seconds())
 # --------------
@@ -52,7 +53,7 @@ async def on_member_join(member):
         user_id = member.id
         time = member.joined_at
         msg_log = (user_id, time)
-        db.write(msg_log)
+        write(msg_log)
 
 
 # ready イベント
@@ -67,7 +68,7 @@ async def kicker():
         # 現在時刻を取得する
         now = datetime.utcnow() + DELTA_JST
         # データベースを参照する
-        for row in db.read_all():
+        for row in read_all():
             # 非アクティブ期間を求める
             term = now - str_to_datetime(row[1])
             if term.days > 5:
@@ -76,7 +77,7 @@ async def kicker():
                 try:
                     await target.kick()
                     print('Kicked ' + target.name)
-                    db.delete(row[0])
+                    delete(row[0])
                 # ターゲットをキックできなかったとき、エラーメッセージを出力する
                 except (discord.errors.Forbidden, AttributeError) as error_content:
                     try:
@@ -104,36 +105,30 @@ def str_to_datetime(str_time):
     return datetime_time
 
 
-# データベース
-class Db:
-    def __init__(self, database_name):
-        self.db = database_name
-
-    # レコードを書き込む
-    def write(self, msg_log):
-        with closing(sqlite3.connect(self.db)) as conn:
-            c = conn.cursor()
-            c.execute('CREATE TABLE IF NOT EXISTS log (id PRIMARY KEY, time)')
-            c.execute('INSERT OR REPLACE INTO log VALUES (?, ?)', msg_log)
-            conn.commit()
-
-    # レコードを読み込む
-    def read_all(self):
-        with closing(sqlite3.connect(self.db)) as conn:
-            c = conn.cursor()
-            c.execute('SELECT * FROM log')
-            return c.fetchall()
-
-    # レコードを削除する
-    def delete(self, user_id):
-        with closing(sqlite3.connect(self.db)) as conn:
-            c = conn.cursor()
-            c.execute('DELETE FROM log WHERE id=?', (user_id, ))
-            conn.commit()
+# レコードを書き込む
+def write(msg_log):
+    with closing(psycopg2.connect(dsn)) as conn:
+        c = conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS log (id PRIMARY KEY, time)')
+        c.execute('INSERT OR REPLACE INTO log VALUES (?, ?)', msg_log)
+        conn.commit()
 
 
-DATABASE = 'activelog.db'
-db = Db(DATABASE)
+# レコードを読み込む
+def read_all():
+    with closing(psycopg2.connect(dsn)) as conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM log')
+        return c.fetchall()
+
+
+# レコードを削除する
+def delete(user_id):
+    with closing(psycopg2.connect(dsn)) as conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM log WHERE id=?', (user_id, ))
+        conn.commit()
+
 
 client.run(TOKEN)
 
